70e3b37672f0a072cfc41eca21de4174a26d1901
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-29 22:11:51 +0530

    final commit

---
+++ database/db.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql2';
+
+export const db = mysql.createConnection({
+    host:process.env.HOST,
+    user:process.env.USER,
+    password:process.env.PASSWORD,
+    database:'blogschema'
+})
